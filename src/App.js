ba09a7433a30b0ae065263428a07003f2fe5b3c4
Sandeep <sandeepypb@gmail.com>
2022-03-22 18:32:39 +0530

    refactor: 💡 apply the global theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
+import Button from "./lib/Button";
+import { ThemeProvider, LightTheme, DarkTheme } from "./lib/styles";
 
 function App() {
+  const [theme, setTheme] = React.useState(true);
+
   return (
-    <>
-      Geeky UI
-    </>
+    <ThemeProvider theme={theme ? LightTheme : DarkTheme}>
+      <Button onClick={() => setTheme(!theme)}>
+        Toggle Theme
+      </Button>
+
+
+
+    </ThemeProvider>
   );
 }
 
